d04931d609dcb58e54b793526037366dd958f705
Antoine Stalin <antoine.stalin@prestataires.boulanger.com>
2021-01-26 01:23:03 +0100

    deploy

---
+++ precache-manifest.c6f120f43d5fecf321312c55424154cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cf7cdd6a579f1d8e93",
+    "url": "/buddy-challenges/css/app.1d0c4c57.css"
+  },
+  {
+    "revision": "8fec10dffb8b663597d8",
+    "url": "/buddy-challenges/css/chat.42d9cec6.css"
+  },
+  {
+    "revision": "f39ed827197f64796e14",
+    "url": "/buddy-challenges/css/shaker.44f730cd.css"
+  },
+  {
+    "revision": "6dbbc059d9c984a29d5c7cb43134761e",
+    "url": "/buddy-challenges/index.html"
+  },
+  {
+    "revision": "8f438a10c5af9cca3ad0",
+    "url": "/buddy-challenges/js/about.1cae3710.js"
+  },
+  {
+    "revision": "d6cf7cdd6a579f1d8e93",
+    "url": "/buddy-challenges/js/app.9fd6f542.js"
+  },
+  {
+    "revision": "8fec10dffb8b663597d8",
+    "url": "/buddy-challenges/js/chat.27c74304.js"
+  },
+  {
+    "revision": "d257a82b37bfe9ad3ed3",
+    "url": "/buddy-challenges/js/chunk-vendors.dad1ed7d.js"
+  },
+  {
+    "revision": "f39ed827197f64796e14",
+    "url": "/buddy-challenges/js/shaker.94d30793.js"
+  },
+  {
+    "revision": "772bb487fe2d972f3eafc0fd121e39da",
+    "url": "/buddy-challenges/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/buddy-challenges/robots.txt"
+  }
+]);
